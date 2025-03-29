1f6c2624bb32b33cf6931bdbf2a61ca3fd3c81c6
Pritam <pritamrajput1993@gmail.com>
2025-03-29 15:32:06 +0530

    working commit

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
 import { Server, ServerCredentials } from '@grpc/grpc-js';
 
-function main() {
+async function main() {
   const server = new Server();
-  server.bind("127.0.1.1:50051", ServerCredentials.createInsecure());
-  console.log("Server running on port 50051");
+  server.bindAsync("127.0.0.1:50051", ServerCredentials.createInsecure(), (err, port) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`Server running on http://localhost:${port}`);
+  });
 }
-main();
+
+main().catch(console.error)
--- server_build/index.js
+++ server_build/index.js
@@ -1,9 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const grpc_js_1 = require("@grpc/grpc-js");
 function main() {
-    const server = new grpc_js_1.Server();
-    server.bind("127.0.1.1:50051", grpc_js_1.ServerCredentials.createInsecure());
-    console.log("Server running on port 50051");
+    return __awaiter(this, void 0, void 0, function* () {
+        const server = new grpc_js_1.Server();
+        server.bindAsync("127.0.0.1:50051", grpc_js_1.ServerCredentials.createInsecure(), (err, port) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(`Server running on http://localhost:${port}`);
+        });
+    });
 }
-main();
+main().catch(console.error);
